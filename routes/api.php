346a5e9bb34d6a1a16e3e6c38f38063b4247a2ee
Carlos Quijano <quijanoo12345@gmail.com>
2023-09-25 07:34:01 -0600

    events

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,11 @@
 <?php
 
-use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EventController;
+
+
 
 
 
@@ -11,10 +14,29 @@ use Illuminate\Support\Facades\Route;
 Route::post('register', [AuthController::class,'register']);
 Route::post('login', [AuthController::class,'login']);
 
+Route::prefix('event')->group(function () {
+    Route::get('/', [EventController::class, 'getEvents']);
+    Route::get('/{id}', [EventController::class, 'getEventsById']);
+    Route::post('/insert', [EventController::class, 'insertEvents']);
+    Route::put('/update/{id}', [EventController::class, 'updateEvents']);
+    Route::delete('/delete/{id}', [EventController::class, 'deleteEvents']);
+});
+
+
+
+
+
+
+
 Route::group(['middleware' => ['auth:sanctum']], function (){
     Route::get('user-profile', [AuthController::class,'userProfile']);
     Route::post('user-profile', [AuthController::class,'userProfile']);
     Route::post('logout', [AuthController::class,'logout']);
 
 });
+
+
+
+
+
 
